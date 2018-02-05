5cdcb83efe12faeb843ba915eec11d353bee7c73
Bitnarae Kim <bxk631@cs.bham.ac.uk>
2018-02-05 12:07:48 +0000

    Upload setting class

---
+++ Survival Jungle/Command/Setting.java
@@ -0,0 +1,14 @@
+package Command;
+
+import javax.swing.JPanel;
+
+public class Setting extends Command{
+
+	@Override
+	public JPanel execute() {
+		// TODO Auto-generated method 
+		System.out.println("Setting");
+		return null;
+	}
+
+}
