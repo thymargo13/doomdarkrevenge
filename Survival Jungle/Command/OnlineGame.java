9079e2fe6ee94d399c402f3763a7b6e8f8c4857d
Khang Foong <ksf794@student.bham.ac.uk>
2018-02-05 12:22:21 +0000

    Online Module

---
+++ Survival Jungle/Command/OnlineGame.java
@@ -0,0 +1,14 @@
+package Command;
+
+import javax.swing.JPanel;
+
+public class OnlineGame extends Command{
+
+	@Override
+	public JPanel execute() {
+		// TODO Auto-generated method stub
+		System.out.println("Online Game");
+		return null;
+	}
+
+}
