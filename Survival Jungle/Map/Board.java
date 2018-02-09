52b4af372d9af723e7273bbeaf4f604790af3eee
lxy545 <lxy545@bham.ac.uk>
2018-02-09 16:22:16 +0000

    player+gamestate

--- Survival Jungle/Map/Board.java
+++ Survival Jungle/Map/Board.java
@@ -1,10 +1,7 @@
 package Map;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -12,62 +9,41 @@ import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-import Entity.Mouse;
-import Entity.Player;
-import Entity.foodonMap;
-
 public class Board extends JPanel implements ActionListener {
-
-	private final int B_WIDTH = 1600;
-	private final int B_HEIGHT = 1200;
-	private final int DELAY = 25;
+	private final int DELAY = 10;	// milliseconds delay
 	private Timer timer;
-	private foodonMap food1;
-	private Player player;
+	private GameState gameState;
 	
-	private int i=0;
-
 	public Board() {
 		setLayout(null);
 		initBoard();
 		addMouseMotionListener(new MouseAdapter() {
 			public void mouseMoved(MouseEvent e) {
-				player.setDx(e.getX());
-				player.setDy(e.getY());
+//				System.out.println("X: "+e.getX()+", Y: "+e.getY());
+				gameState.mouseMoved(e);	// pass the mouse event to game state
 			}
 		});
 	}
 
 	private void initBoard() {
-		player = new Mouse();
-		food1 = new foodonMap();
-		setBackground(new Color(0, 195, 0));
-		setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
-		setDoubleBuffered(true);
-		timer = new Timer(DELAY, this);
+		gameState = new GameState();
+		setPreferredSize(new Dimension(800, 600));	// can use setSize() if component's parent has no layout manager
+		setDoubleBuffered(true);	// Sets whether this component should use a buffer to paint
+		timer = new Timer(DELAY, this);	// Every DELAY ms the timer will call the actionPerformed()
 		timer.start();
+		
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-
-		draw(g);
-	}
-
-	private void draw(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-		food1.draw(g2d, this);
-		player.draw(g2d, this);
-		
-//		g2d.rotate(Math.toRadians(i++), player.getX(), player.getY());
-//		g2d.drawImage(player.getImage(), player.getX()-50, player.getY()-50, 100, 100, this);
-//		Toolkit.getDefaultToolkit().sync();
+		gameState.initDraw(g, this);	// pass the graphics to game state to control all rendering
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		player.move();
+		gameState.update();
+		gameState.draw();
 		repaint();
 	}
 }
