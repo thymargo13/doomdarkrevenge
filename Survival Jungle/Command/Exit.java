cfb0a48e5228f504a6ea64450e0cd39abe82f8e8
bxk631 <bxk631@cs.bham.ac.uk>
2018-03-01 11:29:04 +0000

    sound settings

--- Survival Jungle/Command/Exit.java
+++ Survival Jungle/Command/Exit.java
@@ -12,6 +12,10 @@ public class Exit extends Command {
 		int i = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit the game?", "Confirm exit", JOptionPane.YES_NO_OPTION);
 		if (i == JOptionPane.YES_OPTION) {
 			System.exit(0);
+		} else if(i == JOptionPane.NO_OPTION){
+			System.out.println("No button clicked");
+		} else if (i == JOptionPane.CANCEL_OPTION){
+			System.out.println("Cancel button clicked");
 		}
 		return null;
 	}
--- Survival Jungle/Command/Setting.java
+++ Survival Jungle/Command/Setting.java
@@ -1,13 +1,22 @@
 package Command;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import Audio.Audio_player;
 
 public class Setting extends Command{
 
+	@SuppressWarnings("null")
 	@Override
 	public JPanel execute() {
 		// TODO Auto-generated method 
-		System.out.println("Setting");
+		Audio_player audio = null;
+		int i = JOptionPane.showConfirmDialog(null, "Turn sound OFF?");
+		if(i == JOptionPane.YES_OPTION){
+			audio.stop();
+		}
+		
 		return null;
 	}
 
