fab9b50fe4cddc098130c0a144f186b9814f431d
Haotian <hxy719@student.bham.ac.uk>
2018-03-21 15:44:21 +0000

    error

--- Survival Jungle/Local/Mud.java
+++ Survival Jungle/Local/Mud.java
@@ -45,18 +45,21 @@ public class Mud {
 	 */
 	public void Update() {
 		for (Cell cell : Cell.cells) {
-			if(cell.currentLv == cell.level.get(1)) {
-				
-			}else {
+			
 				if(checkCollide(cell.x,cell.y)) {
-					double dx = (this.colX - this.x);
-					double dy = (this.colY - this.y);
-					cell.x += (dx) * 1 / 100;
-					cell.y += (dy) * 1 / 100;
+					if(cell.currentLv == cell.level.get(0)) {
+						
+					}else {
+						double dx = (this.colX - this.x);
+						double dy = (this.colY - this.y);
+						cell.x += (dx) * 1 / 100;
+						cell.y += (dy) * 1 / 100;
+					}
+					
 				}
 			}
 				
-		}
+		
 	}
 	/**
 	 * 
